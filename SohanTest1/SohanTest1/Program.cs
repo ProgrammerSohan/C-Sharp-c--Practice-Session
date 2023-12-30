6431615b412f4bdedbdd81a4862b353f762bcc30
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-30 08:13:11 +0600

    first testing code

---
+++ SohanTest1/SohanTest1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SohanTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Sohan For C#");
+            Console.WriteLine("I always love to code");
+
+            Console.ReadKey();
+        }
+    }
+}
